afe7a8ad258a845c2dc2809442271e87a581d13f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-23 16:40:17 -0500

    Update and rename Home.py to home.py

---
+++ home.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Novus Energy')
+
+st.write("Bienvenidos al futuro energético 👋")
+
+st.markdown(
+  """
+  ####Tecnología para la optimización
+  Planes de Ahorro o Repotenciación
+  ###Tecnología para la neutralidad de carbono
+  Planes de Mitigación
+  ###Tecnología para salvar el planeta
+  Planes de Adaptación
+  """
+)
