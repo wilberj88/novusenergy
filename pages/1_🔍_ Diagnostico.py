002e85532bff983ddd50402bda5e4014d786987b
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-24 15:27:41 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -3,10 +3,12 @@ import pandas as pd
 import numpy as np
 
 st.title('Diagnosticamos tu potencial ahorro energético')
-st.write('Indica las ubicaciones que requieres y el presupuesto anual disponible')
+st.write('Indica la ubicación de la necesidad energética y el presupuesto anual disponible')
 
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
+
+st.text_input("¿Cuál es tu presupuesto anual para consumo energético?", key="presupuesto")
