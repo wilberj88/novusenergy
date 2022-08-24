c3ec90406e12181984dbb4020baf2e77253b8216
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-24 15:21:11 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -2,4 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.write('Diagnosticamos tu potencial ahorro energético')
+st.title('Diagnosticamos tu potencial ahorro energético')
+st.write('Indica las ubicaciones que requieres y el presupuesto anual disponible')
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
