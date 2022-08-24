10f865588ee6ff3d3b565687bfb5ddcc6435cfb8
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-24 07:39:49 -0500

    Update home.py

--- home.py
+++ home.py
@@ -2,7 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Energy", page_icon=":battery:")
+
 st.title('Novus Energy')
+st.header("Salvemos juntos el planeta")
 
 st.write("Bienvenidos al futuro energético 👋")
 
