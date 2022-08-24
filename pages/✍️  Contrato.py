de8e4c68bda39ff768f865b9c5a04ea54aaeced6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-24 15:50:17 -0500

    Update ✍️  Contrato.py

--- pages/✍️  Contrato.py
+++ pages/✍️  Contrato.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write('Conoce nuestro propuesta contractual inteligente')
+st.title('Tenemos un contrato 📜  personalizado 🎯 a tu diagnóstico 🔎 ')
+st.write('Porque tu necesidad es única, también tu contrato')
+st.markdown("""
+BLA BLA BLA BLA
+""")
+st.text_input("Incorpore su firma")
+
+st.text_input("Incorpore su correo electrónico para envío de factura")
+st.write('Gracias por confiar en los servicios de Novus Energy 🔋')
