a1674bfe04c12d48b7897d73f29702d97d91d75c
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-08-24 15:37:54 -0500

    Update 2_🛒_  Servicios.py

--- pages/2_🛒_  Servicios.py
+++ pages/2_🛒_  Servicios.py
@@ -3,5 +3,4 @@ import streamlit as st
 st.title('Conoce nuestro portafolio de servicios personalizado 🛒 a tu Diagnóstico 🔎')
 
 st.write('Dado que tu presupuesto es: ')
-st.write(st.session_state.presupuesto)
 st.write('Te recomendamos:')
