09ce2e22386ae08269a4e1878e6480d4b28a3dcd
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-09 17:55:38 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -1,11 +1,24 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import ipywidgets as widgets
 
 st.title('Diagnosticamos tu potencial ahorro energético')
 
 
 st.text_input("¿Cuál es tu consumo energético mensual?")
+widgets.IntSlider(
+    value=7,
+    min=0,
+    max=10,
+    step=1,
+    description='Test:',
+    disabled=False,
+    continuous_update=False,
+    orientation='horizontal',
+    readout=True,
+    readout_format='d'
+)
 
 st.text_input("¿Cuál es tu presupuesto anual para consumo energético?")
 
