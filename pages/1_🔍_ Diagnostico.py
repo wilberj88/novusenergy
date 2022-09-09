5cc67d1252d6891330114daeb00026eaf936d509
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-09 18:25:12 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -8,8 +8,9 @@ st.title('Diagnosticamos tu potencial ahorro energético')
 st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000)
 
 st.slider('En pesos cuánto es tu presupuesto energético anual?', 0, 1000000000)
+st.select_slider('Pick a size', ['S', 'M', 'L'])
 
-st.select_slider('¿Cuál es tu fuente principal de energía?', ['Agua', 'Combustible', 'Calor'])
+# st.select_slider('¿Cuál es tu fuente principal de energía?', ['Agua', 'Combustible', 'Calor'])
 
 
 # st.write('Indica la ubicación de la necesidad energética y el presupuesto anual disponible')
