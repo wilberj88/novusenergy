dbde2edc181d0209afd4c14245cea642ff0bbd76
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-09 18:20:36 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -9,7 +9,7 @@ st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?'
 
 st.slider('En pesos cuánto es tu presupuesto energético anual?', 0, 1000000000)
 
-st.selectbox('¿Cuál es tu fuente principal de energía?', ['agua', 'combustible', 'calor', 'carbón', 'termoeléctrica'])
+st.selectbox('¿Cuál es tu fuente principal de energía?', ['agua', 'combustible', 'calor', 'carbon', 'termoelectrica'])
 
 
 
