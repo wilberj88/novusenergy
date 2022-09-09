dafc79da8dfe6209cfc7034a0a1a3a376ae24453
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-09 18:32:51 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -8,13 +8,3 @@ st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?'
 
 st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 100000000)
 
-st.radio('Pick one', ['cats', 'dogs'])
-
-# st.select_slider('¿Cuál es tu fuente principal de energía?', ['Agua', 'Combustible', 'Calor'])
-
-
-# st.write('Indica la ubicación de la necesidad energética y el presupuesto anual disponible')
-# map_data = pd.DataFrame(
- #   np.random.randn(1000, 2) / [50, 50] + [35.56, -74.04],
- #   columns=['lat', 'lon'])
-# st.map(map_data)
