0e3a20dbe85dbe35e98532926b0a851de0437668
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-19 08:11:47 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -4,9 +4,9 @@ import streamlit as st
 st.title('Diagnosticamos tu potencial ahorro energético')
 
 
-st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
+a = st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
 
-st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 100000000, key="presupuesto")
+b= st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 100000000, key="presupuesto")
 
 
-st.radio('Cuál es tu fuente principal de energia', ['agua', 'carbon', 'combustible', 'termoelectrica', 'solar', 'viento'], key="tipo")
+c= st.radio('Cuál es tu fuente principal de energia', ['agua', 'carbon', 'combustible', 'termoelectrica', 'solar', 'viento'], key="tipo")
