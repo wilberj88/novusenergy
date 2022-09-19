f4ea352fd1fd36420e5e74273bba4e19cbe2c376
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-19 09:02:57 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -6,6 +6,6 @@ st.title('Diagnosticamos tu potencial ahorro energético')
 
 st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
 
-st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 100000000, key="presupuesto")
+st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 1000000, key="presupuesto")
 
 st.radio('Cuál es tu fuente principal de energia', ['agua', 'carbon', 'combustible', 'termoelectrica', 'solar', 'viento'], key="tipo")
