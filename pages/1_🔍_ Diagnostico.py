61b16a8c2a62e739b8c791415e6557148dc57883
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-19 08:32:41 -0500

    Update 1_🔍_ Diagnostico.py

--- pages/1_🔍_ Diagnostico.py
+++ pages/1_🔍_ Diagnostico.py
@@ -4,8 +4,8 @@ import streamlit as st
 st.title('Diagnosticamos tu potencial ahorro energético')
 
 
-a = st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
-st.sesion_state['consumo']=a
+a= st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
+st.session_state['consumo']=a
 
 b= st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 100000000, key="presupuesto")
 
