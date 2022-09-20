7d528853856084991780c60c2632ce9105406221
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-20 09:05:56 -0500

    Update 2_🛒_  Servicios.py

--- pages/2_🛒_  Servicios.py
+++ pages/2_🛒_  Servicios.py
@@ -2,6 +2,16 @@ import streamlit as st
 import numpy as np
 
 st.title('Tu Diagnóstico🔎 nos indica cuánto podemos ahorrarte💰:')
+
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+
 st.write('En 1 año:')
 
 st.write(st.session_state.presupuesto)
