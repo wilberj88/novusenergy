4c4f7a97ecf19999f6fce68cbbae6dfdfff66084
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-20 15:23:50 -0500

    Update 2_🛒_  Servicios.py

--- pages/2_🛒_  Servicios.py
+++ pages/2_🛒_  Servicios.py
@@ -6,28 +6,16 @@ st.title('Tu Diagnóstico🔎 nos indica que podemos ahorrarte💰:')
 st.header('Ahorro en el año 1 (%)')
 st.write([5, 9, 14, 17]) # see *
 
-st.header('Ahorro en el año 5')
+st.header('Ahorro en el año 5 (%)')
 st.write([20, 36, 56, 68]) # see *
-
-st.latex(r''' e^{i\pi} + 1 = 0 ''')
-st.text('Fixed width text')
-st.markdown('_Markdown_') # see *
-st.write('Most objects') # df, err, func, keras!
-st.write(['st', 'is <', 3]) # see *
-st.title('My title')
-
-st.subheader('My sub')
-
-st.write('En 1 año:')
 
 st.write(st.session_state.presupuesto)
 
+st.write(st.session_state.consumo)
 
-st.write('En 5 años:')
 
-st.write('El portafolio de servicios personalizado 🛒 a tu necesidad🎯 es:')
+st.write(st.session_state.fuente)
 
-st.write('BLA BLA BLA')
 
 st.write('Empecemos ahora 🕰')
 
@@ -35,7 +23,3 @@ st.write('¿Cómo materializaremos el ahorro?')
 st.write('Transformaremos tu matriz energética hacia fuentes no solo más económicas sino también más sostenibles:')
 st.write('Matriz Energética Actual Vs Proyectada')
 
-st.write(st.session_state.consumo)
-
-
-st.write(st.session_state.fuente)
