9149d396286e6dd819422cd94cedbd298c21a424
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-09-21 07:29:43 -0500

    Update ✍️  Contrato.py

--- pages/✍️  Contrato.py
+++ pages/✍️  Contrato.py
@@ -2,9 +2,14 @@ import streamlit as st
 
 st.title('Tenemos un contrato 📜  personalizado 🎯 a tu diagnóstico 🔎 ')
 st.write('Porque tu necesidad es única, también tu contrato')
-st.markdown("""
-BLA BLA BLA BLA
-""")
+
+with st.form(key='my_form'):
+   username = st.text_input('Username')
+   password = st.text_input('Password')
+   st.form_submit_button('Login')
+
+st.button('Descargar Contrato Inteligente')
+
 st.text_input("Incorpore su firma si está de acuerdo con las condiciones")
 
 st.text_input("Incorpore su correo electrónico para envío de factura")
