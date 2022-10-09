1d2facc18d33e51ea4f87a71398946438bc82df5
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-09 11:10:14 -0500

    Create recomedacion.py

---
+++ pages/recomedacion.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Energy", page_icon="🔋")
+
+st.title('Novus Energy 🔋')
+st.header("Sistemas de Ángulos Óptimos para Páneles Solares ☀️")
+
+st.write("Sistema para optimización de Sol ☀️")
+
+
+st.write("Sistema para optimización de Lluvia 🌧️")
+
+
+st.write("Sistema para optimización de Viento 🌪️")
