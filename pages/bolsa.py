7b23269c43f6259912026e4d8debc29cf0164e01
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-09 11:18:59 -0500

    Create bolsa.py

---
+++ pages/bolsa.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
+st.set_page_config(layout="wide", page_title="Novus Energy", page_icon="🔋")
+
+st.title('Novus Energy 🔋')
+st.header("Monitor de Precios de Energía en Tiempo Real ⚡")
+st.write("Información desde XM")
