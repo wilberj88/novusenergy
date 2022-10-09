d00bb834fc44544d19a5bbdf2089dcb834c1abee
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2022-10-09 11:23:39 -0500

    Update acciones.py

--- pages/acciones.py
+++ pages/acciones.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from yahoofinance import HistoricalPrices
 
 # SETTING PAGE CONFIG TO WIDE MODE AND ADDING A TITLE AND FAVICON
 st.set_page_config(layout="wide", page_title="Novus Energy", page_icon="🔋")
@@ -8,3 +9,5 @@ st.set_page_config(layout="wide", page_title="Novus Energy", page_icon="🔋")
 st.title('Novus Energy 🔋')
 st.header("Monitor de Acciones de Empresas Energéticas ⚡")
 st.write("Información desde Yahoo Finance")
+
+req = HistoricalPrices('AAPL')
