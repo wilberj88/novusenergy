4456c715cc1484e354937fe87ed31dadebe3fe58
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2023-11-11 23:53:32 +0100

    Update home.py

--- home.py
+++ home.py
@@ -7,40 +7,45 @@ st.set_page_config(layout="wide", page_title="Novus Energy", page_icon="🔋")
 
 st.title('Novus Energy 🔋')
 st.header("Real time Monitoring, Operations & Simulations")
-st.selectbox("Choose a City", ["New York", "Bogota", "Madrid", "London"])
-st.subheader("1. Monitoring")
-st.write("Energy Demand by City")
-st.write("Energy Supply by City")
+a = st.selectbox("Choose a City", ["New York", "Bogota", "Madrid", "London"])
 
-st.subheader("2. Operations")
-st.write("Optimization by Current Weather")
-
-st.subheader("3. Simulations")
-st.write("Estimate potential costs of Weather and Climate Dissasters")
-
-
-st.write("Bienvenidos al futuro energético 👋")
-
-st.markdown(
-  """
-  En esta web encontrarás:
-  - 🔎 _    Simulación de Escenarios Energéticos
-  - 🛒 _    Modelación en Tiempo Real de Demanda y Oferta Energética
-  - ✍️ _     Alarmas de Precios, Disponibilidades y Rentabilidades
+if a:
+  st.subheader("1. Monitoring")
+  col1, col2 = st.columns(2)
+  with col1:
+    st.write("Energy Demand")
+  with col2:
+    st.write("Energy Supply")
   
-  Todo lo anterior basado en:
-  - Tecnología para la optimización: Planes de Ahorro o Repotenciación
-  - Tecnología para la neutralidad de carbono: Planes de Mitigación
-  - Tecnología para salvar el planeta: Innovación en toda la cadena energética y Planes de Adaptación
+  st.subheader("2. Operations")
+  st.write("Optimization by Current Weather")
   
-  EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
-  """
-)
-
-
-#st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
-
-#st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 1000000, key="presupuesto")
-
-#st.radio('¿Cuál es tu fuente principal de energia?', ['Agua', 'Carbon', 'Combustible', 'Termoelectrica', 'Solar', 'Viento'], key="fuente")
-
+  st.subheader("3. Simulations")
+  st.write("Estimate potential costs of Weather and Climate Dissasters")
+  
+  
+  st.write("Bienvenidos al futuro energético 👋")
+  
+  st.markdown(
+    """
+    En esta web encontrarás:
+    - 🔎 _    Simulación de Escenarios Energéticos
+    - 🛒 _    Modelación en Tiempo Real de Demanda y Oferta Energética
+    - ✍️ _     Alarmas de Precios, Disponibilidades y Rentabilidades
+    
+    Todo lo anterior basado en:
+    - Tecnología para la optimización: Planes de Ahorro o Repotenciación
+    - Tecnología para la neutralidad de carbono: Planes de Mitigación
+    - Tecnología para salvar el planeta: Innovación en toda la cadena energética y Planes de Adaptación
+    
+    EMPIEZA TU 🔎 DIAGNÓSTICO AHORA 🕰
+    """
+  )
+  
+  
+  #st.slider('En kilovatios hora (Kwh): ¿cuál es tu consumo energético mensual?', 0, 100000, key="consumo")
+  
+  #st.slider('En millones de pesos al año: ¿cuánto inviertes en energía?', 0, 1000000, key="presupuesto")
+  
+  #st.radio('¿Cuál es tu fuente principal de energia?', ['Agua', 'Carbon', 'Combustible', 'Termoelectrica', 'Solar', 'Viento'], key="fuente")
+  
